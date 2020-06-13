385b5a48c70afdbefb29330788e6bea41e47545c
Mateus Peschke <67184@PROWAY.TREINA>
2020-06-13 11:47:48 -0300

    Aula02_For com lista

---
+++ Aula02/03_Repetição_02.py
@@ -0,0 +1,15 @@
+lista_idades = [10,15,18,45,52,56,54,21]
+for i in range(5):
+    print(i)
+
+# Perccorrendo a lista com "Foreach"
+print(lista_idades)
+for i in lista_idades:
+    print(i)
+print('\n')
+
+# Percorrendo a lista com for e range
+for i in range(len(lista_idades)):
+    print(lista_idades[i])
+
+    
