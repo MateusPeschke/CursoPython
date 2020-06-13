b6779564ab44c4a1e449347453c5f631596db65c
Mateus Peschke <67184@PROWAY.TREINA>
2020-06-13 10:34:21 -0300

    Aula02 - Laço repetição FOR

---
+++ Aula02/Repeticao.py
@@ -0,0 +1,26 @@
+# Estrutura de Repetição "for"
+
+# Estrutura de código repetida
+# print('Aluno', 1)
+# print('Aluno', 2)
+# print('Aluno', 3)
+# print('Aluno', 4)
+
+# # Laço 'for'com a função de 1-5
+# for i in range (1,6):
+#     print('Aluno', i)
+
+# print("\n")
+
+# # Laço 'for' com a função range apenas com limite final
+# for i in range(5):
+#     print('Aluno', i+1)
+
+# Cadastri com repetição
+for i in range (2):
+    nome = input('Digite seu nome: ')
+    sobrenome = input('Digite seu sobrenome: ')
+    idade = int (input('Digite sua idade: '))
+    print('\n')
+    print('Usuario Cadastrado:',nome, sobrenome,'\nIdade: ', idade )
+    print('\n')
