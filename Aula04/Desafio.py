bd0fb16e3e8d37b35d258d43d6f0bf2b5f4bf0f2
Mateus Peschke <67184@PROWAY.TREINA>
2020-07-04 12:24:46 -0300

    Aula04_Resolusao_desafio

--- Aula04/Desafio.py
+++ Aula04/Desafio.py
@@ -2,51 +2,14 @@
 # criar um caixa que mostre a quantidado de notas para completar a quantidade requerida!
 
 
+dinheiro = [100,50,20,10,5,1,0.5,0.1,0.05,0.01]
+valor = float(input("Digite o valordesejado: "))
+for pila in dinheiro:
+    quantidade = int(valor // pila)
+    valor = round(valor % pila,2)
+    # valor = valor - (pila * quantidade)
 
-valor_requerido = float(input('Digite o valor requerido :'))
-quant001 = 0
-quant100 = 0
-quant50 = 0
-quant20 = 0
+    print(f"{quantidade:d} nota(s) valor {pila:>6.2f} "
+        if pila > 1 else
+            f"{quantidade:d} moeda(s) valor {pila:>6.2f}")
 
-
-while True:
-
-    if valor_requerido >= 100:
-        valor_requerido = valor_requerido - 100
-        quant100 = quant100 + 1
-    if valor_requerido >= 50:
-        valor_requerido = valor_requerido - 50
-        quant50 = quant50 + 1
-    if valor_requerido >= 20:
-        valor_requerido = valor_requerido - 20
-        quant20 = quant20 + 1
-    if valor_requerido >= 10:
-        valor_requerido = valor_requerido - 10
-        quant10 = quant10 + 1
-    if valor_requerido >= 5:
-        valor_requerido = valor_requerido - 5
-        quant5 = quant5 + 1
-    if valor_requerido >= 2:
-        valor_requerido = valor_requerido - 2
-        quant2 = quant2 + 1
-    if valor_requerido >= 1:
-        valor_requerido = valor_requerido - 1
-        quant1 = quant1 + 1
-    if valor_requerido >= 0.5:
-        valor_requerido = valor_requerido - 0.5
-        quant105 = quant05 + 1
-    if valor_requerido >= 0.25:
-        valor_requerido = valor_requerido - 0.25
-        quant025 = quant025 + 1
-    if valor_requerido >= 0.1:
-        valor_requerido = valor_requerido - 0.1
-        quant01 = quant01 + 1
-    if valor_requerido >= 0.05:
-        valor_requerido = valor_requerido - 0.05
-        quant005 = quant005 + 1
-    if valor_requerido >= 0.01:
-        valor_requerido = valor_requerido - 0.01
-        quant001 = quant001 + 1
-
-    print()
